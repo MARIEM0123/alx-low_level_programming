11a58f53e69464bde6cc3f445dd2e5f49a603bde
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-20 16:52:02 -0700

    C language task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-void main(void)
+#include<unistd.h>
+int main (void)
 {
-	return ("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+ write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19", 57);
+ return (1);
 }
+
