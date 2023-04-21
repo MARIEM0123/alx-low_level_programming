7d8646edef57acee63cf9a5afedc12925e5d07f0
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-21 16:18:51 -0700

    Variables, if, else, while task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	int a;
+
 	while (a < 10)
 	{
-	putchar(a+48);
+	putchar(a + 48);
 	a++;
 	}
 	for (a=0; a < 6; a++)
 	{
-	putchar(a+97);
+	putchar(a + 97);
 	}
 	putchar('\n');
 	return (0);
