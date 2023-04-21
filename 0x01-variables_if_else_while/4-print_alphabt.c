401e60bb06ed5b844e8fb0f122f4ac71884d2011
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-21 13:49:02 -0700

    C - Variables, if, else, while task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - C language is so cool
+ * Return: Our return here is 0
+ */
+int main(void)
+{
+        char ch = 'a';
+
+        while (ch <= 'z'&& ch != q && ch != e)
+        {
+        putchar(ch);
+        ch++;
+        }
+        putchar('\n');
+        return (0);
+}i
