1b39188202227d6009f3d24abdc71a4832fac8a0
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-21 14:43:35 -0700

    Variables, if, else, while task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - C language is so cool
+ * Return: Our return here is 0
+ */
+int main(void)
+{
+	int  a = 0;
+
+        while (a < 10)
+        {
+        putchar(a);
+        a++;
+        }
+        putchar('\n');
+        return (0);
+}
