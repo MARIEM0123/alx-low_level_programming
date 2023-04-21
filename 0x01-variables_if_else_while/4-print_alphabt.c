197a8d9ddbd8c7c63ed272080f3041edc42c027b
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-21 14:00:04 -0700

    C - Variables, if, else, while task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,16 @@ int main(void)
 {
         char ch = 'a';
 
-        while (ch <= 'z'&& ch != 'q' && ch != 'e')
+        while (ch <= 'z')
         {
+	if (ch != 'e' && ch != 'q')
         putchar(ch);
         ch++;
         }
+	else
+	{
+	ch++;
+	}
         putchar('\n');
         return (0);
 }
