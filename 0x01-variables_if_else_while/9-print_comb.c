3ad00783ea2f0c1d8f3fdab0c05343eef18dabb8
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-21 16:40:21 -0700

    C - Variables, if, else, while task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - C language is so cool
+ * Return: Our return here is 0
+ */
+int main(void)
+{
+	int a = 0;
+	int i=0;
+	while (a < 10)
+	{
+	putchar(a + 48);
+	a++;
+	if (i < 9)
+	putchar(',');
+	putchar(' ');
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
