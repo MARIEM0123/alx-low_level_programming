18aa0df128f0894bcfb842c5500a947e4823c59e
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-21 15:41:36 -0700

    Variables, if, else, while task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - C language is so cool
+ * Return: Our return here is 0
+ */
+int main(void)
+{
+        char ch = 'z';
+        
+        while (ch => 'a')
+        {
+        putchar(ch);
+        ch++;
+        }
+        putchar('\n');
+        return (0);
+}
