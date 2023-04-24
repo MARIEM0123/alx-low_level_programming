ffa68d31c18d37799496a26179774ac76d0b16ef
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-24 03:46:23 -0700

    Variables, if, else, while task13

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - C is so cool
+ * Return: The return here is 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	a = 0;
+	while (a < 10)
+	{
+	b = 0;
+	while (b < 10)
+	{
+	c = a;
+	while (c < 10)
+	{
+	d=b+1;
+	while(d < 10)
+	{
+	if (a==0 && b==a && c==a && d==a)
+	d++;
+	else
+	{
+	putchar(a + 48);
+	putchar(b + 48);
+	putchar(' ');
+	putchar(c + 48);
+	putchar(d +48);
+	if (a + 48 == 57 && b + 48 == 56 && c == a && d == a)
+	break;
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	d++;
+	}
+	c++;
+	}
+	b++;
+	}
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
+
