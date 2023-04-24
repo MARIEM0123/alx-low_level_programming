303e7af1e2c0d7726b3ef69b2df5ad422f2f5ef2
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-24 02:21:39 -0700

    Variables, if, else, while task12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,38 +7,47 @@
 int main(void)
 {
 	int a, b, c;
-	
-	c=0;
-	while (c < 10)
-	{
-	a=1;
+
+	a = 0;
 	while (a < 10)
 	{
-	b = 2;
+	b = 1;
 	while (b < 10)
 	{
-	if (b <= a)
+	if (a>=b)
 	{
 	b++;
 	}
 	else
 	{
-	putchar(c + 48);
-	putchar (a + 48);
+	c=2;
+	while (c < 10)
+	{
+	if (c <=b){
+	c++;
+	}
+	else
+	{
+	putchar(a + 48);
 	putchar(b + 48);
-	if (a + 48 == 56 && b + 48 == 57)
+	putchar(c + 48);
+	if (a + 48 == 55 && b + 48 == 56 && c + 48 == 57)
+	{
 	break;
+	}
 	else
 	{
 	putchar(',');
 	putchar(' ');
-	b++;
 	}
+	c++;
+	}
+	}
+	b++;
 	}
 	}
 	a++;
 	}
 	putchar('\n');
-	}
 	return (0);
 }
