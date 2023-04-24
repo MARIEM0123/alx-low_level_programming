c5cc1a1134926a836450bb90c5b0ff188d49c7a3
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-24 08:33:29 -0700

    Variables, if, else, while task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,19 @@ int main(void)
 	int a, b;
 
 	a = 0;
-	while (a <=99)
+	while (a <= 99)
 	{
 	b = a;
-	while (b <=99)
+	while (b <= 99)
 	{
 	if (b != a)
 	{
-	putchar((a/10)+48);
-	putchar((a%10)+48);
+	putchar((a / 10) + 48);
+	putchar((a % 10) + 48);
 	putchar(' ');
-	putchar((b/10)+48);
-	putchar((b%10)+48);
-	if (a !=98 ||b != 99)
+	putchar((b / 10) + 48);
+	putchar((b % 10) + 48);
+	if (a != 98 || b != 99)
 	{
 	putchar(',');
 	putchar(' ');
