b6bcc1932273245bbd3a87bc0357beb70c54b551
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-25 09:58:30 -0700

    Functions, nested loops task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,25 @@
+#include <unistd.h>
 #include <stdio.h>
 /**
  * main - C is so cool
  * Return: The return here is 0
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void print_alphabet(void)
 {
 	char a = 'a';
 	while (a <= 'z')
 	{
-	putchar(a);
+	_putchar(a);
 	a++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
 int main(void)
 {
-        print_alphabet();
-        return (0);
+	print_alphabet();
+	return (0);
 }
