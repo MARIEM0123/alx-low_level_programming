89a17c4b16d13f290008f516c973c918ba84efcf
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-25 15:53:47 -0700

    t6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * int_abs - C is so cool
+ * _abs - C is so cool
+ * @a: to give the absolute value
  * Return: The return here is 0
  */
 int _abs(int a)
