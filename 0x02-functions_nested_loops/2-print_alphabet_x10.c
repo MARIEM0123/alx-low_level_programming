5ab89838921c5b40a67d84f3fcd7e908cc45a212
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-25 12:44:42 -0700

    Functions, nested loops task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - C is so cool
+ * Return: The return here is 0
+ */
+void print_alphabet_x10(void)
+{
+	char a, x;
+
+	x = 0;
+	while (x < 10)
+	{
+	a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	x++;
+	}	
+}
