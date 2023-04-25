6b5a38f78e7011aea8b462945f3c0f620d4e3403
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-25 14:52:31 -0700

    t4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - C is so cool
- * @c:to check if an alphabet 
+ * _isalpha - C is so cool
+ * @c: to check if an alphabet 
  * Return: The return here is 0
  */
 int _isalpha(int c)
 {
 	if ((c <= 90 && c >= 65) || (c <= 122 && c >= 97))
-	return(1);
+	return (1);
 	else
 	return (0);
 }
