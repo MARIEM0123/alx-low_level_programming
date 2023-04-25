1dff47189d6c6a87833bb5ba0cb667550209697c
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-25 16:57:23 -0700

    t8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - C is so cool
+ * Return: The return here is 0
+ */
+void jack_bauer(void)
+{
+int a, b, c, d;
+for (a = 0; a <= 2; a++)
+{
+for (b = 0; a <= 9; b++)
+{
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+{
+for (b = 0; a <= 5; c++)
+{
+for (b = 0; a <= 9; d++)
+{
+_putchar(a + '0');
+_putchar(a + '0');
+_putchar(58);
+_putchar(a + '0');
+_putchar(a + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
+}
