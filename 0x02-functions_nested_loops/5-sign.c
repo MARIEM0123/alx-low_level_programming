2518b9853f2c4e20151b9901228909bc434143af
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-25 15:26:49 -0700

    t5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,26 @@
 #include <time.h>
 #include <stdio.h>
+#include "main.h"
 /**
- * main - C is so cool
+ * print_sign - C is so cool
+ * @n: to verify the sign
  * Return: The return here is 0
  */
 int print_sign(int n)
 {	
 	if (n > 0)
 	{
-	putchar('+');
-	putchar (',');
+	putchar (43);
 	return (1);
 	}
 	else if (n==0)
 	{
-	putchar('0');
-	putchar (',');
+	putchar(48);
 	return(0);
 	}
 	else
 	{
-	putchar ('-');
-	putchar (',');
+	putchar (45);
 	return(-1);
 	}
 }
