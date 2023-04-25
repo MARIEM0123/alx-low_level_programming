f0262fce629a66b187dd603349df7567138ffbb5
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-25 09:20:29 -0700

    Functions, nested loops task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,21 +3,19 @@
  * main - C is so cool
  * Return: The return here is 0
  */
- void print_alphabet(void)
+ void print_alphabet(char a)
 {
-	char a;
-	
 	a = 'a';
 	while (a <= 'z')
   	{
 	putchar(a);
 	a++;
 	}
+	putchar('\n');
 }
 int main(void)
 {
-	print_alphabet();
-	putchar('\n');
+	print_alphabet();i
 	return (0);
 }
 
