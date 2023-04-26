7d1322feda369214261cb83b023ca9632f9e7f6f
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-25 17:36:05 -0700

    t10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
-
-
-
-
+/**
+ * _abs - C is so cool
+ * @a: to give the absolute value
+ * Return: The return here is 0
+ */
 int add(int a, int b)
 {
-return ( a+ b);
+return (a + b);
 }
