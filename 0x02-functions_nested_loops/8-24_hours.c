def305e267ef029f0e2db0ffb5ff19642baa180b
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-25 17:06:23 -0700

    t8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,15 +13,15 @@ for (b = 0; a <= 9; b++)
 {
 if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
-for (b = 0; a <= 5; c++)
+for (c = 0; c <= 5; c++)
 {
-for (b = 0; a <= 9; d++)
+for (d = 0; d <= 9; d++)
 {
 _putchar(a + '0');
-_putchar(a + '0');
+_putchar(b + '0');
 _putchar(58);
-_putchar(a + '0');
-_putchar(a + '0');
+_putchar(c + '0');
+_putchar(d + '0');
 _putchar('\n');
 }
 }
