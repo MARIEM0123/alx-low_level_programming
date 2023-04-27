50c761aeda80052f61390208a153a4f464972044
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-27 01:12:14 -0700

    t0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+include "main.h"
+
+int _isupper(int a)
+{
+	if(a>= 65 && a <= 90)
+	{
+		return(1);
+	}
+	else
+		retturn(0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#More functions, more nested loops
+Continue on your learning, we have more loop functions for you
