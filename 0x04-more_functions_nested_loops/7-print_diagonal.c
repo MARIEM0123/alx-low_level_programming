87097b4297f3dd9556f5ec2c9837f5ac228d7f91
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-27 12:33:28 -0700

    t7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_diagonal - C is so cool
+ * @n: the parameter to be considered
+ * Return: The return here is 0
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	j = 0;
+	for (j = 0; j < n; j++)
+{
+	_putchar (92);
+	_putchar ('\n');
+	_putchar (' ');
+	for (i = 0; i < j ; i++)
+		_putchar (' ');
+}
+_putchar ('\n');
+}
