1288ab1d1425e29d1577ecc0f7eea174c88fd03d
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-27 10:13:03 -0700

    t3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_numbers - C is so cool
+ * @i: the number to consider
+ * Return: The return here is 0
+ */
+
+void print_numbers(void)
+{
+	int i; 
+	for (i = 0; i <=9; i++)
+		_putchar (i + '0');
+}
