ca42524e0253dcf69008e84f11e31d751284b9d7
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-27 12:06:24 -0700

    t6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_line - C is so cool
+ * @n: the parameter to be considered
+ * Return: The return here is 0
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+	for (j = 0; j < n; j++)
+{
+	putchar (95);
+}
+putchar ('\n');
+}
+}
