e01629d40adc323870ea4f36340b709ad6426f20
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-27 13:08:47 -0700

    t7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,18 +11,18 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-		_putchar ('\n')
+		_putchar ('\n');
 	else
 	{
-	j = 0;
-	for (j = 0; j < n; j++)
+	for (j = 1; j < n; j++)
 {
 	_putchar (92);
 	_putchar ('\n');
-	_putchar (' ');
 	for (i = 0; i < j ; i++)
 		_putchar (' ');
 }
+	_putchar (92);
+	_putchar ('\n');
+
 }
-_putchar ('\n');
 }
