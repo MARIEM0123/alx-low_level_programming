a33308f381083fecde6c96af69ab8968d24c2db9
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-27 10:00:12 -0700

    t2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,13 @@
 #include "main.h"
 #include<stdio.h>
+
 /**
  * mul - C is so cool
- * @a: the number to consider
+ * @a: the first number to consider
+ * @b: the second number to consider
  * Return: The return here is 0
  */
+
 int mul(int a, int b)
 {
 	return (a * b);
