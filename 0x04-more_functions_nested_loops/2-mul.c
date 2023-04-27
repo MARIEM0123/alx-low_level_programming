d7091aa9d42fa09ae2f1a31f2ed884d61e90b952
MARIEM01234 <mariem.sotradema@gmail.com>
2023-04-27 09:53:22 -0700

    t2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * mul - C is so cool
+ * @a: the number to consider
+ * Return: The return here is 0
+ */
+int int mul(int a, int b)
+{
+	return (a * b);
+}
