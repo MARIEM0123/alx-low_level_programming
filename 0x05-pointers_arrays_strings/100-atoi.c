5be403feced4d3f2b5815cd37185dfa1232911ce
MARIEM01234 <mariem.sotradema@gmail.com>
2023-05-02 15:24:18 -0700

    t10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _atoi - check the code
+ * @s: the parameter
+ * Return: there is a return
+ */
+
+int _atoi(char *s)
+{
+	int a, b, c, d, f, g;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	f = 0;
+	g = 0;
+	while (s[d] != '\0')
+		d++;
+	while (a < d && f == 0)
+	{
+		if (s[a] == '-')
+		++b;
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			g = s[a] - '0';
+			if (d % 2)
+				g = -g;
+			c = c * 10 + g;
+			f = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			f = 0;
+		}
+		a++;
+	}
+	if (f == 0)
+		return (0);
+	return (c);
+}
+
