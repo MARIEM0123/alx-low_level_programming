1eaed2ed1468c077ca0c27529e64e6b7ac783e8e
MARIEM01234 <mariem.sotradema@gmail.com>
2023-05-02 00:40:58 -0700

    t0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * reset_to_98 - changing to 98
+ * @*n: the parameter
 void reset_to_98(int *n)
 {
-        *n = 408;
+	*n = 408;
 
-        *n = 98;
+	*n = 98;
 }
