29b150008c680f5e25d8d5e44edc6ea9d620c572
MARIEM01234 <mariem.sotradema@gmail.com>
2023-05-12 11:24:54 -0700

    static librairies

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - test if is upper letter
+ * @c: the parameter
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+	return (0);
+}
