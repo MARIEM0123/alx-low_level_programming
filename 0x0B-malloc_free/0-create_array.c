0e7f55e4705e192ff86a425b961fc7b14c0ba647
MARIEM01234 <mariem.sotradema@gmail.com>
2023-05-16 11:57:41 -0700

    ma

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,14 @@ char *create_array(unsigned int size, char c)
 {
 	char *s;
 	unsigned int i;
-
+	
 	s = malloc(sizeof(char) * size);
+	if (size == 0)
+		return (NULL);
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		s[i] = c;
 	return (s);
-	if (size == 0 || s == NULL)
-                return (NULL);
-}
 
+}
