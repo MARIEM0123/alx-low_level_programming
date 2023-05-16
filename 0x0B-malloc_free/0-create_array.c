5578f073f99402a27b8b67b2947488021faff8e7
MARIEM01234 <mariem.sotradema@gmail.com>
2023-05-16 11:59:30 -0700

    ma

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - the funcion 
+ * create_array - the funcion
  * @size: the parameter
  * @c: the parameter
  * Return: value or NULL
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *s;
 	unsigned int i;
-	
+
 	s = malloc(sizeof(char) * size);
 	if (size == 0)
 		return (NULL);
