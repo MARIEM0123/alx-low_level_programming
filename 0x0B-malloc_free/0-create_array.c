8dd693dfb851d44e2910fbb78d7acaa235b0c84a
MARIEM01234 <mariem.sotradema@gmail.com>
2023-05-16 06:30:20 -0700

    memoryallocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	for (i = 0; i < size; i++)
 		s[i] = c;
 	return (s);
-	if (size == 0 || s == NULL)
+	if (size == 0)
                 return (NULL);
 }
 
