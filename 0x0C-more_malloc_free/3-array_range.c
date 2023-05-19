a39637b84f343207c30c55a51139ab352a24fb65
MARIEM01234 <mariem.sotradema@gmail.com>
2023-05-19 13:49:21 -0700

    more memory allocation functions

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,10 +3,9 @@
 
 /**
  * *array_range - creates an array of integers
- * @min: minimum range of values stored
- * @max: maximum range of values stored and number of elements
- *
- * Return: pointer to the new array
+ * @min: minimum
+ * @max: maximum
+ * Return: NULL or pointer
  */
 int *array_range(int min, int max)
 {
