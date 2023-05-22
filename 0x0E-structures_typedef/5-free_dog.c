fb54b9ca765f45ca256727b9fbb69c67bac9f7d2
MARIEM01234 <mariem.sotradema@gmail.com>
2023-05-22 11:16:45 -0700

    Structures

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated
+ * @d: structure
+ */
+void free_dog(dog_t *d)
+{
+		free(d->name);
+		free(d->owner);
+		free(d);
+}
