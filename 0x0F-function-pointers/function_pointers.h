989bafbd4a338c008ca39901a93eb6dcb97018e1
MARIEM01234 <mariem.sotradema@gmail.com>
2023-05-24 12:30:41 -0700

    p

--- 0x0F-function-pointers/function_pointers.h
+++ 0x0F-function-pointers/function_pointers.h
@@ -1,6 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
+#ifndef _HEADER_
+#define _HEADER_
 #include <stddef.h>
 #include <stdlib.h>
 
@@ -10,4 +9,3 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 #endif
-
