be54760e3f86fb39be565358e4564d3ec43ffd44
MARIEM0123 <m00riemhissou@gmail.com>
2023-06-01 11:59:10 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
-
+void _constructor(void) __attribute__ ((constructor));
 /**
- * first - add a sentence
+ * _constructor - add a sentence
  */
-void first(void)
+void _constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
