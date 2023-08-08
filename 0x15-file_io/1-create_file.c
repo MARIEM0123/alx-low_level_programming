95506e93d3d1ef81297b2f1c85a65e056c0cdb70
MARIEM0123 <m00riemhissou@gmail.com>
2023-08-08 02:03:33 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		text_content = "";
-	for (i = 0; text_content[i]; i++);
+
+	for (i = 0; text_content[i]; i++)
+		;
 	rt = write(file, text_content, i);
 
 	if (rt == -1)
