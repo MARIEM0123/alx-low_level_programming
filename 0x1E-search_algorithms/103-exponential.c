3a3def3534ccc7b1feed6d88b08aa24874e6a8c9
MARIEM0123 <m00riemhissou@gmail.com>
2024-01-16 10:13:30 -0800

    search algo

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,10 +1,10 @@
 #include "search_algos.h"
 
 /**
- * print_array - The funct is to prints the contents of an array.
- * @array: The first arg is the source of the array to print.
- * @l: The second arg is the left index of the array.
- * @r: The third arg is the right index of the array.
+ * print_array - The funct is to prints the array contents
+ * @array: The first arg is the source
+ * @l: The second arg is the left index
+ * @r: The third arg is the right index
  */
 void print_array(int *array, size_t l, size_t r)
 {
@@ -19,12 +19,12 @@ void print_array(int *array, size_t l, size_t r)
 }
 
 /**
- * binary_search_index1 - The funct is to find a value in a sorted array using \
- * @array: The first arg is the array to search in.
- * @l: The second arg is the left index of the array.
- * @r: The third arg is the right index of the array.
- * @value: The return is the value to look for.
- * Return: The return is the first index of the value in the array or -1.
+ * binary_search_index1 - The funct is to find a value in the array
+ * @array: The first arg is the array
+ * @l: The second arg is the left index
+ * @r: The third arg is the right index
+ * @value: The return is the value
+ * Return: The return is the first index of the value or -1.
  */
 int binary_search_index1(int *array, size_t l, size_t r, int value)
 {
@@ -51,11 +51,11 @@ int binary_search_index1(int *array, size_t l, size_t r, int value)
 }
 
 /**
- * exponential_search - The funct to find a value in a sorted array using \
- * @array: The first arg is the array to search in.
- * @size: The second arg is the length of the array.
- * @value: The third arg is the value to look for.
- * Return: The return is the index of the value in the array or -1.
+ * exponential_search - The funct to find a value in the array
+ * @array: The first arg is the array
+ * @size: The second arg is the length
+ * @value: The third arg is the value
+ * Return: The return is the index of the value or -1.
  */
 int exponential_search(int *array, size_t size, int value)
 {
